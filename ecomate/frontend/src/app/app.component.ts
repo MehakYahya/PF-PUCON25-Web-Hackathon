a0ecc65ae3a6c49225490b213b276c74c7ebba22
Mehak Yahya <mehakyahya81@gmail.com>
2025-06-30 23:38:54 +0500

    App component

---
+++ ecomate/frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet,RouterLink,RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+}
