58e9ab9a16c86ff95f5357f58113a70473920886
Mehak Yahya <mehakyahya81@gmail.com>
2025-06-30 18:08:16 +0500

    Update activityRoutes.js

--- ecomate/backend/routes/activityRoutes.js
+++ ecomate/backend/routes/activityRoutes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const auth = require('../middleware/authMiddleware');
 const Activity = require('../models/Activity');
 
+// Log new activity
 router.post('/', auth, async (req, res) => {
   try {
     const { type, amount, emission } = req.body;
 
     const activity = new Activity({
-      user: req.user,
+      user: req.user.id, 
       type,
       amount,
       emission
@@ -22,14 +23,16 @@ router.post('/', auth, async (req, res) => {
   }
 });
 
-
+// Get activity history
 router.get('/', auth, async (req, res) => {
   try {
-    const activities = await Activity.find({ user: req.user }).sort({ createdAt: -1 }); // latest first
+    const activities = await Activity.find({ user: req.user.id }) 
+      .sort({ createdAt: -1 });
     res.json(activities);
   } catch (error) {
     console.error('Error fetching activity history:', error);
     res.status(500).json({ message: 'Server error' });
   }
 });
+
 module.exports = router;
